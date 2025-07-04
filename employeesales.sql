dd0d6590cc7c100f86d1cde07c2263eb743756cb
Nandini Ch <54891298+ch-nandini@users.noreply.github.com>
2025-07-04 14:04:22 -0700

    Add files via upload

---
+++ employeesales.sql
@@ -0,0 +1,11 @@
+insert into employee_sales.employeesales (emp_id, emp_name, department, sale_month, sales_amount)
+values  (1, 'Alice', 'Electronics', '2024-01', 5000),
+        (2, 'Bob', 'Electronics', '2024-01', 6000),
+        (3, 'Charlie', 'Electronics', '2024-01', 6000),
+        (4, 'David', 'Clothing', '2024-01', 4500),
+        (5, 'Eva', 'Clothing', '2024-01', 4000),
+        (6, 'Frank', 'Clothing', '2024-01', 4500),
+        (7, 'Grace', 'Electronics', '2024-02', 7000),
+        (8, 'Heidi', 'Clothing', '2024-02', 5000),
+        (9, 'Ivan', 'Clothing', '2024-02', 5200),
+        (10, 'Judy', 'Electronics', '2024-02', 6800);
